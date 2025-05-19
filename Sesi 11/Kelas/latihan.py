13988d32ddbbe0b7b509b39c65a49fc9cdf58fbb
Imamabdurohman <imamabdurohman0310@gmail.com>
2025-05-19 15:01:33 +0700

    Tugas

--- Sesi 11/Kelas/latihan.py
+++
@@ -1,7 +0,0 @@
-def KonversiSuhu(suhu, dari ="C", ke ="R"):
-    if (dari == "C" and ke == "R"):
-        return 4/5*suhu
-
-print(KonversiSuhu(100),"C","K")
-
-# Koversi dari 100 C adalah 373,13 Kelvin
