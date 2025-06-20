03d60e2b855f7c1b7ad7ec2d6364762589262b43
Imamabdurohman <imamabdurohman0310@gmail.com>
2025-06-20 18:29:03 +0700

    Tugas

--- siteapp/polls/views.py
+++ siteapp/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def alamat_view(request):
-    return HttpResponse("Alamat: Jl. Simpang Dago No. 17, Jampang Kulon")
+    return HttpResponse("Alamat: Jl. KH. Mama Oyon No. 11, Cicantayan Sukabumi")
 
 def telepon_view(request):
-    return HttpResponse("Telepon: +62 857-2132-9929")
+    return HttpResponse("Telepon: +62 838-0604-5240")
